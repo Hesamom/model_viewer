2a05f9ea0811f438d5e1e41e815b011d70ba336a
DESKTOP-JO8FSQH\Hesam <Hesamom@live.com>
2023-11-05 17:35:25 +0330

    issue #1 - Fix shared lib link error at runtime

--- main.cpp
+++ main.cpp
@@ -31,7 +31,6 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.8f, 1.0f );
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         
         /* Swap front and back buffers */
