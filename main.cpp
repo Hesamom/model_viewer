c7a97473a0df1617968867dbe3498b8396d9445a
DESKTOP-JO8FSQH\Hesam <Hesamom@live.com>
2023-11-08 19:12:41 +0330

    issue #3
    - config cmake to copy 'res' folder to bin
    - remove treat warning as errors to avoid std and 3rd party warnings stopping builds

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 
 int main()
 {
-    /*
+    //an example of loading a shader 
+    //'res' folder in the source directory is copied to bin folder in each build to be accessible 
     using namespace modelViewer::res;
     shaderLoader loader;
     std::filesystem::path path("res/shaders/sample.glsl");
-    auto shader = loader.load(path, shaderType::fragment);*/
+    auto shader = loader.load(path, shaderType::fragment);
+    
     
     window window(500, 500, "Playground");
     return 0;
