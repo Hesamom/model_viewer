a4bb9e8f86852bb299adb6b56399c1e704831b43
Wolf <Wolf@DESKTOP-LACDQ0V>
2024-02-24 15:07:18 +0330

    issue #43
    fix bug that we can't load renderdoc.dll,
    we should put renderdoc.dll on C(windows driver):\Windows\System32 path.

--- src/window/renderdoc.cpp
+++ src/window/renderdoc.cpp
@@ -2,38 +2,32 @@
 #include "renderdoc.h"
 
 
-void renderdoc::init()
-{
-	if(HMODULE mod = GetModuleHandle("renderdoc.dll"))
-	{
+void renderdoc::init(){
+	if(HMODULE mod = LoadLibrary("renderdoc.dll")){
 		pRENDERDOC_GetAPI RENDERDOC_GetAPI =
 			(pRENDERDOC_GetAPI)GetProcAddress(mod, "RENDERDOC_GetAPI");
 		int ret = RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_1_2, (void **)&rdoc_api);
 		assert(ret == 1);
-		
-		if (ret == 1)
-		{
+
+		if (ret == 1){
 			std::cout << "initiated renderdoc successfully!\n";
 		}
 	}
-	else
-	{
+	else{
 		std::cerr << "failed to initiate renderdoc!\n";
+		std::cerr << GetLastError() <<"\n";
 	}
 }
 
-void renderdoc::startCapture()
-{
-	if (rdoc_api)
-	{
+void renderdoc::startCapture(){
+	if (rdoc_api){
+		rdoc_api->SetActiveWindow();
 		rdoc_api->StartFrameCapture(NULL, NULL);
 	}
 }
 
-void renderdoc::EndCapture()
-{
-	if (rdoc_api)
-	{
+void renderdoc::EndCapture(){
+	if (rdoc_api){
 		rdoc_api->EndFrameCapture(NULL, NULL);
 	}
 }
